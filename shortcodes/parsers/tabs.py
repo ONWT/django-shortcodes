2367ad8a630af6abf4922791b4d566b19ee9c2c3
noobidoo <ttraskback@gmail.com>
2014-10-07 01:35:56 +0300

    Added template and fixed tab parsing

--- shortcodes/parsers/tabs.py
+++ shortcodes/parsers/tabs.py
@@ -1,5 +1,6 @@
 __author__ = 'Noobs'
 import re
+import uuid
 from django.template import Template, Context
 from django.template.loader import render_to_string
 from django.conf import settings
@@ -9,19 +10,24 @@ from shortcodes.parser import construct_regex, __parse_args__
 def parse(kwargs, template_name="shortcodes/tabs.html"):
 		Tabs_id = kwargs.get('id')
 		Tab_Content = kwargs.get('shortcodeContent')
-		if Tabs_id:
+		tabs = []
+
+		if not Tabs_id:
+				Tabs_id = "Tab"+str(uuid.uuid4())
+		if Tab_Content:
 				ex = re.compile(construct_regex(['tab']))
 				groups = ex.findall(Tab_Content)
-				tabs = {}
 				tabcount = 0
 				for tab in groups:
-						args = __parse_args__(args)
-						title = args['title']
-						tab_id = Tabs_id + tabcount
-						tabs[title] = (tab_id, title, tab[5])
+						args = __parse_args__(tab[2])
 
-				ctx = {
-					'tab_id': Tabs_id,
-					'tabs': tabs
-				}
-				return render_to_string(template_name, ctx)
+						title = args['title']
+						tab_id = Tabs_id + str(tabcount)
+						tabs.append((tab_id, title, tab[4]))
+						tabcount += 1
+
+		ctx = {
+			'tab_id': Tabs_id,
+			'tabs': tabs
+		}
+		return render_to_string(template_name, ctx)
